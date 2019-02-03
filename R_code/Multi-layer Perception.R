6130ef342ac8470e52144fbf45fa41ff12b400bf
whChoi <achieve0410@gmail.com>
2019-02-03 16:29:22 +0900

    0203 code

--- R_code/Multi-layer Perception.R
+++ R_code/Multi-layer Perception.R
@@ -320,10 +320,13 @@ library(MASS)
 use_session_with_seed(1, disable_parallel_cpu = FALSE)
 
 ## read csv file for model
-m_score <- read.csv("cluster_origin_10.csv")
+m_score <- read.csv("driving_score_180ea_modify.csv")
 
 ## read csv file for data
-d_score <- read.csv("cluster_tmap.csv")
+d_score <- read.csv("tmp_data.csv")
+
+dim(m_score)
+dim(d_score)
 
 x_train <- as.matrix(m_score[, -4])
 y_train <- as.matrix(m_score[, 4])
@@ -334,13 +337,13 @@ y_test <- as.matrix(d_score[, 4])
 model = keras_model_sequential()
 model %>%
   layer_dense(input_shape = ncol(x_train), units = 128, activation = "relu") %>%
-  layer_dropout(rate = 0.1) %>%
+  layer_dropout(rate = 0.05) %>%
   layer_dense(units = 128, activation = "relu") %>%
-  layer_dropout(rate = 0.1) %>%
+  layer_dropout(rate = 0.05) %>%
   layer_dense(units = 64, activation = "relu") %>%
-  layer_dropout(rate = 0.1) %>%
+  layer_dropout(rate = 0.05) %>%
   layer_dense(units = 32, activation = "relu") %>%
-  layer_dropout(rate = 0.1) %>%
+  layer_dropout(rate = 0.05) %>%
   layer_dense(units = 16, activation = "relu") %>%
   layer_dense(units = 1)
 
